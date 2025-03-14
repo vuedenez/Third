c0a9f4a922a5dad6e79b7ae1cc831f3a013da091
vuedenez <vpglazkova@gmail.com>
2025-03-14 16:39:03 +0300

    Revert "Fix paths and build configuration for GitHub Pages"
    This reverts commit 9ed8619274871075e7dac9fe182a1ff48428ec5c.

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Third/',
-  assetsInclude: ['**/*.JPG', '**/*.PNG', '**/*.jpg', '**/*.png'],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        '404': resolve(__dirname, 'public/404.html')
-      }
-    }
-  },
-  publicDir: 'public'
+  assetsInclude: ['**/*.JPG', '**/*.PNG', '**/*.jpg', '**/*.png']
 }) 
